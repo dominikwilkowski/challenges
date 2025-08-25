d45e3e040f86f551060d9c8bdc5b764f1201c5ba
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-08-26 05:30:49 +1000

    tweaks

--- ranges/src/range.rs
+++ ranges/src/range.rs
@@ -23,7 +23,7 @@ pub struct Range {
 }
 
 impl Range {
-	pub fn new(input: &String) -> Result<Self, RangeError> {
+	pub fn new(input: &str) -> Result<Self, RangeError> {
 		let mut numbers = input
 			.split(',')
 			.map(|number| {
@@ -40,26 +40,46 @@ impl Range {
 	pub fn get_range(&self) -> Vec<Ranges> {
 		let mut output = Vec::new();
 		let mut start_range = None;
+		let mut iterations = 0;
 
-		let mut iter = self.numbers.iter().peekable();
-		while let Some(num) = iter.next() {
+		if self.numbers.len() == 1 {
+			return vec![Ranges::Scalar(self.numbers[0])];
+		}
+
+		for num in &self.numbers {
 			if start_range.is_none() {
 				start_range = Some(*num);
 			} else if let Some(start) = start_range {
-				if let Some(next) = iter.peek() {
-					println!("num={num} next={next}");
-					if *num < *next - 1 {
-						if start < *num {
-							output.push(Ranges::Range { from: start, to: *num });
-							start_range = Some(**next);
-						} else {
-							output.push(Ranges::Scalar(start));
-						}
+				iterations += 1;
+				println!("start={start} num={num} iterations={iterations} range={}", *num == start + iterations);
+				if start != *num - iterations {
+					println!("!!!");
+					if start < *num && *num == start + iterations {
+						output.push(Ranges::Range { from: start, to: *num });
+					} else {
+						output.push(Ranges::Scalar(start));
 					}
+					start_range = Some(*num);
+					iterations = 0;
 				}
 			}
 		}
 
+		println!(
+			"start={start_range:?} num={} iterations={iterations} range={}",
+			self.numbers[self.numbers.len() - 1],
+			self.numbers[self.numbers.len() - 1] == start_range.unwrap() + iterations
+		);
+		if let Some(start) = start_range {
+			if start < self.numbers[self.numbers.len() - 1] && self.numbers[self.numbers.len() - 1] == start + iterations {
+				output.push(Ranges::Range {
+					from: start,
+					to: self.numbers[self.numbers.len() - 1],
+				});
+			} else {
+				output.push(Ranges::Scalar(self.numbers[self.numbers.len() - 1]));
+			}
+		}
 		output
 	}
 }
@@ -70,25 +90,25 @@ mod test {
 
 	#[test]
 	fn range_test() {
-		let range = Range::new(&String::from("1,2,3,4,5")).unwrap();
+		let range = Range::new("1,2,3,4,5").unwrap();
 		assert_eq!(range.get_range(), vec![Ranges::Range { from: 1, to: 5 }]);
 
-		let range = Range::new(&String::from("6,3,4,5")).unwrap();
+		let range = Range::new("6,3,4,5").unwrap();
 		assert_eq!(range.get_range(), vec![Ranges::Range { from: 3, to: 6 }]);
 	}
 
 	#[test]
 	fn scalar_test() {
-		let range = Range::new(&String::from("1")).unwrap();
+		let range = Range::new("1").unwrap();
 		assert_eq!(range.get_range(), vec![Ranges::Scalar(1)]);
 
-		let range = Range::new(&String::from("5,3,1")).unwrap();
+		let range = Range::new("5,3,1").unwrap();
 		assert_eq!(range.get_range(), vec![Ranges::Scalar(1), Ranges::Scalar(3), Ranges::Scalar(5)]);
 	}
 
 	#[test]
 	fn mixed_test() {
-		let range = Range::new(&String::from("1,3,4,8,10")).unwrap();
+		let range = Range::new("1,3,4,8,10").unwrap();
 		assert_eq!(
 			range.get_range(),
 			vec![
@@ -99,7 +119,7 @@ mod test {
 			]
 		);
 
-		let range = Range::new(&String::from("10,3,4,5,1")).unwrap();
+		let range = Range::new("10,3,4,5,1").unwrap();
 		assert_eq!(range.get_range(), vec![Ranges::Scalar(1), Ranges::Range { from: 3, to: 5 }, Ranges::Scalar(10)]);
 	}
 }
