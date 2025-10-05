9ea46d773cb3cf6da12a4177d132b456c4e911c5
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-10-06 05:07:20 +1000

    the tinest server

--- http/src/main.rs
+++ http/src/main.rs
@@ -1,3 +1,26 @@
+use std::{
+	io::prelude::*,
+	net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) {
+	let response = "HTTP/1.1 200 OK\n\
+		Content-Length: 13\n\
+		Content-Type: text/plain\n\
+		\n\
+		Hello, world!";
+
+	let _ = stream.write_all(response.as_bytes());
+}
+
 fn main() {
-	println!("Hello, world!");
+	let port = 7878;
+	let listener = TcpListener::bind(format!("127.0.0.1:{port}")).unwrap();
+
+	println!("Listening on http://127.0.0.1:{port}");
+
+	for stream in listener.incoming() {
+		let stream = stream.unwrap();
+		handle_connection(stream);
+	}
 }
