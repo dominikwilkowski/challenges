457ce3eae7e22a2ee11218b5f69b001e979d7b5f
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-10-23 07:00:15 +1000

    added tests and finished API outline

--- trains/src/main.rs
+++ trains/src/main.rs
@@ -3,7 +3,41 @@ mod railroad;
 use railroad::Railroad;
 
 fn main() {
-	println!("Hello, world!");
+	let railroad = Railroad::new()
+		.add_route("A", "B", 5)
+		.add_route("B", "C", 4)
+		.add_route("C", "D", 8)
+		.add_route("D", "C", 8)
+		.add_route("D", "E", 6)
+		.add_route("A", "D", 5)
+		.add_route("C", "E", 2)
+		.add_route("E", "B", 3)
+		.add_route("A", "E", 7);
+
+	// 1. The distance of the route A-B-C.
+	// 2. The distance of the route A-D.
+	// 3. The distance of the route A-D-C.
+	// 4. The distance of the route A-E-B-C-D.
+	// 5. The distance of the route A-E-D.
+	debug_assert_eq!(railroad.get_distance(vec!["A", "B", "C"]), 9);
+	debug_assert_eq!(railroad.get_distance(vec!["A", "D"]), 5);
+	debug_assert_eq!(railroad.get_distance(vec!["A", "D", "C"]), 13);
+	debug_assert_eq!(railroad.get_distance(vec!["A", "E", "B", "C", "D"]), 22);
+	debug_assert_eq!(railroad.get_distance(vec!["A", "E", "D"]), 13);
+
+	// 6. The number of trips starting at C and ending at C with a maximum of 3 stops.
+	debug_assert_eq!(railroad.get_routes_max_stops("C", "C", 3), 2);
+
+	// 7. The number of trips starting at A and ending at C with exactly 4 stops.
+	debug_assert_eq!(railroad.get_routes_with_stops("A", "C", 4), 1);
+
+	// 8. The length of the shortest route (in terms of distance to travel) from A to C.
+	// 9. The length of the shortest route (in terms of distance to travel) from B to B.
+	debug_assert_eq!(railroad.get_shortest_route_length("A", "C"), 9);
+	debug_assert_eq!(railroad.get_shortest_route_length("B", "B"), 9);
+
+	// 10. The number of different routes from C to C with a distance of less than 30.
+	debug_assert_eq!(railroad.get_number_routes_max_distance("C", "C", 30), 3);
 }
 
 #[cfg(test)]
@@ -11,17 +45,17 @@ mod testing {
 	use super::*;
 
 	#[test]
-	fn input_test() {
-		let railroad = Railroad::new()
-			.route("A", "B", 5)
-			.route("B", "C", 4)
-			.route("C", "D", 8)
-			.route("D", "C", 8)
-			.route("D", "E", 6)
-			.route("A", "D", 5)
-			.route("C", "E", 2)
-			.route("E", "B", 3)
-			.route("A", "E", 7);
+	fn new_test() {
+		let _railroad = Railroad::new()
+			.add_route("A", "B", 5)
+			.add_route("B", "C", 4)
+			.add_route("C", "D", 8)
+			.add_route("D", "C", 8)
+			.add_route("D", "E", 6)
+			.add_route("A", "D", 5)
+			.add_route("C", "E", 2)
+			.add_route("E", "B", 3)
+			.add_route("A", "E", 7);
 
 		// assert routes inputted
 	}
@@ -29,20 +63,85 @@ mod testing {
 	#[test]
 	fn get_distance_test() {
 		let railroad = Railroad::new()
-			.route("A", "B", 5)
-			.route("B", "C", 4)
-			.route("C", "D", 8)
-			.route("D", "C", 8)
-			.route("D", "E", 6)
-			.route("A", "D", 5)
-			.route("C", "E", 2)
-			.route("E", "B", 3)
-			.route("A", "E", 7);
+			.add_route("A", "B", 5)
+			.add_route("B", "C", 4)
+			.add_route("C", "D", 8)
+			.add_route("D", "C", 8)
+			.add_route("D", "E", 6)
+			.add_route("A", "D", 5)
+			.add_route("C", "E", 2)
+			.add_route("E", "B", 3)
+			.add_route("A", "E", 7);
 
 		assert_eq!(railroad.get_distance(vec!["A", "B", "C"]), 9);
 		assert_eq!(railroad.get_distance(vec!["A", "D"]), 5);
 		assert_eq!(railroad.get_distance(vec!["A", "D", "C"]), 13);
 		assert_eq!(railroad.get_distance(vec!["A", "E", "B", "C", "D"]), 22);
 		assert_eq!(railroad.get_distance(vec!["A", "E", "D"]), 13);
+	}
+
+	#[test]
+	fn get_routes_max_stops_test() {
+		let railroad = Railroad::new()
+			.add_route("A", "B", 5)
+			.add_route("B", "C", 4)
+			.add_route("C", "D", 8)
+			.add_route("D", "C", 8)
+			.add_route("D", "E", 6)
+			.add_route("A", "D", 5)
+			.add_route("C", "E", 2)
+			.add_route("E", "B", 3)
+			.add_route("A", "E", 7);
+
+		assert_eq!(railroad.get_routes_max_stops("C", "C", 3), 2);
+	}
+
+	#[test]
+	fn get_routes_with_stops_test() {
+		let railroad = Railroad::new()
+			.add_route("A", "B", 5)
+			.add_route("B", "C", 4)
+			.add_route("C", "D", 8)
+			.add_route("D", "C", 8)
+			.add_route("D", "E", 6)
+			.add_route("A", "D", 5)
+			.add_route("C", "E", 2)
+			.add_route("E", "B", 3)
+			.add_route("A", "E", 7);
+
+		assert_eq!(railroad.get_routes_with_stops("A", "C", 4), 1);
+	}
+
+	#[test]
+	fn get_shortest_route_length_test() {
+		let railroad = Railroad::new()
+			.add_route("A", "B", 5)
+			.add_route("B", "C", 4)
+			.add_route("C", "D", 8)
+			.add_route("D", "C", 8)
+			.add_route("D", "E", 6)
+			.add_route("A", "D", 5)
+			.add_route("C", "E", 2)
+			.add_route("E", "B", 3)
+			.add_route("A", "E", 7);
+
+		assert_eq!(railroad.get_shortest_route_length("A", "C"), 9);
+		assert_eq!(railroad.get_shortest_route_length("B", "B"), 9);
+	}
+
+	#[test]
+	fn get_number_routes_max_distance_test() {
+		let railroad = Railroad::new()
+			.add_route("A", "B", 5)
+			.add_route("B", "C", 4)
+			.add_route("C", "D", 8)
+			.add_route("D", "C", 8)
+			.add_route("D", "E", 6)
+			.add_route("A", "D", 5)
+			.add_route("C", "E", 2)
+			.add_route("E", "B", 3)
+			.add_route("A", "E", 7);
+
+		assert_eq!(railroad.get_number_routes_max_distance("C", "C", 30), 3);
 	}
 }
--- trains/src/railroad.rs
+++ trains/src/railroad.rs
@@ -1,37 +1,34 @@
-pub struct Railroad {}
+pub struct Railroad {/* TODO: add fields */}
 
 impl Railroad {
+	#[must_use]
 	pub fn new() -> Self {
 		Self {}
 	}
 
-	pub fn route(&mut self, from: &str, to: &str, distance: u8) -> Self {
-		todo!("Add routes");
+	#[must_use = "Once routes are added don't forget to calculate something with them"]
+	pub fn add_route(mut self, _from: &str, _to: &str, _distance: u8) -> Self {
+		// TODO: add them routes already!
 		self
 	}
 
-	pub fn get_distance(&self, route: Vec<&str>) -> u8 {
+	pub fn get_distance(&self, _route: Vec<&str>) -> u8 {
 		todo!("Calculate distance");
-		0
 	}
 
-	pub fn get_routes_max_stops(&self, from: &str, to: &str, max_stops: u8) -> u8 {
+	pub fn get_routes_max_stops(&self, _from: &str, _to: &str, _max_stops: u8) -> u8 {
 		todo!("Find number of trips with max stops");
-		0
 	}
 
-	pub fn get_routes_with_stops(&self, from: &str, to: &str, stops: u8) -> u8 {
+	pub fn get_routes_with_stops(&self, _from: &str, _to: &str, _stops: u8) -> u8 {
 		todo!("Find number of trips with exact stops");
-		0
 	}
 
-	pub fn get_shortest_route_length(&self, from: &str, to: &str) -> u8 {
+	pub fn get_shortest_route_length(&self, _from: &str, _to: &str) -> u8 {
 		todo!("Find shortest route");
-		0
 	}
 
-	pub fn get_shortest_routes(&self, from: &str, to: &str) -> u8 {
+	pub fn get_number_routes_max_distance(&self, _from: &str, _to: &str, _max_distance: u8) -> u8 {
 		todo!("Find how many routes");
-		0
 	}
 }
