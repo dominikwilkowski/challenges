2c702877151be0b7f70b54e6df73e5481506c2dd
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-11-06 08:52:29 +1000

    added hound and prepare for DTMF

--- dtmf/src/main.rs
+++ dtmf/src/main.rs
@@ -1,3 +1,8 @@
+use hound::{WavReader, WavSamples};
+
 fn main() {
-	println!("Hello, world!");
+	let mut reader = WavReader::open("touch_tone.wav").unwrap();
+	let spec = reader.spec();
+	let samples = reader.samples::<i16>().filter_map(Result::ok).collect::<Vec<_>>();
+	println!("{spec:?}\n{}", samples.len());
 }
