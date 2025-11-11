d6e53f85d35f1f18f76c493db4e62ef93d568844
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2025-11-12 06:48:02 +1000

    added calc

--- visreq/src/calc.rs
+++ visreq/src/calc.rs
@@ -1,3 +1,44 @@
+pub enum Operator {
+	Add,
+	Subtract,
+	Multiply,
+	Divide,
+}
+
+pub fn parse_line(line: &str) -> (Operator, i64) {
+	let operator = match &line[0..1] {
+		"+" => Operator::Add,
+		"-" => Operator::Subtract,
+		"*" | "x" => Operator::Multiply,
+		"/" | ":" => Operator::Divide,
+		_ => panic!("Invalid operator {}", &line[0..1]),
+	};
+	let operand = &line[1..]
+		.replace(" ", "")
+		.replace("q", "9")
+		.replace("o", "0")
+		.parse::<i64>()
+		.expect(&format!("Number not valid {}", &line[1..].replace(" ", "").replace("q", "9").replace("o", "0")));
+
+	(operator, *operand)
+}
+
 pub fn calc_lines(lines: Vec<String>) -> i64 {
-	todo!("Go line by line, get the operator, then calc. Floor divide!")
+	let mut result = 0;
+
+	for line in lines {
+		println!("{line}");
+		let (operator, operand) = parse_line(&line);
+		match operator {
+			Operator::Add => result += operand,
+			Operator::Subtract => result -= operand,
+			Operator::Multiply => result *= operand,
+			Operator::Divide => {
+				let sub = (result as f64 / operand as f64).floor();
+				result = sub as i64;
+			},
+		}
+	}
+
+	result
 }
