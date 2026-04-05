06cdfe1133f1cbe4b7de837d0a621e301b4c95d1
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2026-04-06 03:16:16 +1000

    finished task

--- rate-limited-task-queue/src/main.rs
+++ rate-limited-task-queue/src/main.rs
@@ -1,15 +1,40 @@
 use rand::RngExt;
-use std::time::Duration;
-use tokio::time::sleep;
+use std::{
+	sync::Arc,
+	time::{Duration, Instant},
+};
+use tokio::{sync::Semaphore, task::JoinSet, time::sleep};
+
+const MAX_CONCURRENT: usize = 5;
 
 #[tokio::main]
 async fn main() {
-	let worker = worker(1).await;
-	println!("{worker}");
+	let start_time = Instant::now();
+	let permits = Arc::new(Semaphore::new(MAX_CONCURRENT));
+	let mut set = JoinSet::new();
+	for id in 1..=20 {
+		let permits = permits.clone();
+		set.spawn(worker(id, permits));
+	}
+
+	set.join_all().await;
+
+	println!("\nDone in {}ms", start_time.elapsed().as_millis());
 }
 
-async fn worker(id: usize) -> String {
+async fn worker(id: usize, permits: Arc<Semaphore>) -> Result<String, ()> {
+	let Ok(_permit) = permits.acquire().await else {
+		eprintln!("Task {} failed to acquire semaphore permit", id);
+		return Err(());
+	};
+
+	let start_time = Instant::now();
+	let in_flight = MAX_CONCURRENT - permits.available_permits();
+	println!("Start ID [{id}] (in_flight: {in_flight})");
+
 	let time = rand::rng().random_range(100..=500);
 	sleep(Duration::from_millis(time)).await;
-	format!("Task {id} completed")
+
+	println!("Finished ID [{id}] in {}ms", start_time.elapsed().as_millis());
+	Ok(format!("Task {id} completed"))
 }
