3b2560dd57cf81900791637ad15bd1ab421fc997
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2026-04-05 10:24:53 +1000

    added silly outline

--- rate-limited-task-queue/src/main.rs
+++ rate-limited-task-queue/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-	println!("Hello, world!");
+use rand::RngExt;
+use std::time::Duration;
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() {
+	let worker = worker(1).await;
+	println!("{worker}");
+}
+
+async fn worker(id: usize) -> String {
+	let time = rand::rng().random_range(100..=500);
+	sleep(Duration::from_millis(time)).await;
+	format!("Task {id} completed")
 }
