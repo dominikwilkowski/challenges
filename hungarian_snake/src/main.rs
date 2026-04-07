744e0554e32cbea2ccb853bcfac5da22754aa223
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2026-04-08 07:01:58 +1000

    made type lookup faster

--- hungarian_snake/src/main.rs
+++ hungarian_snake/src/main.rs
@@ -3,42 +3,20 @@ use std::io::{self, Read};
 fn convert_to_snake(input: &str) -> String {
 	let mut output = String::new();
 
-	let input = input.strip_prefix("u16").unwrap_or(input);
-	let input = input.strip_prefix("u32").unwrap_or(input);
-	let input = input.strip_prefix("u64").unwrap_or(input);
-	let input = input.strip_prefix("i16").unwrap_or(input);
-	let input = input.strip_prefix("i32").unwrap_or(input);
-	let input = input.strip_prefix("i64").unwrap_or(input);
-	let input = input.strip_prefix("f16").unwrap_or(input);
-	let input = input.strip_prefix("f32").unwrap_or(input);
-	let input = input.strip_prefix("f64").unwrap_or(input);
-	let input = input.strip_prefix("dec").unwrap_or(input);
-	let input = input.strip_prefix("dw").unwrap_or(input);
-	let input = input.strip_prefix("ch").unwrap_or(input);
-	let input = input.strip_prefix("fp").unwrap_or(input);
-	let input = input.strip_prefix("dp").unwrap_or(input);
-	let input = input.strip_prefix("rg").unwrap_or(input);
-	let input = input.strip_prefix("sz").unwrap_or(input);
-	let input = input.strip_prefix("st").unwrap_or(input);
-	let input = input.strip_prefix("fn").unwrap_or(input);
-	let input = input.strip_prefix("d").unwrap_or(input);
-	let input = input.strip_prefix("b").unwrap_or(input);
-	let input = input.strip_prefix("i").unwrap_or(input);
-	let input = input.strip_prefix("f").unwrap_or(input);
-	let input = input.strip_prefix("c").unwrap_or(input);
-	let input = input.strip_prefix("f").unwrap_or(input);
-	let input = input.strip_prefix("n").unwrap_or(input);
-	let input = input.strip_prefix("p").unwrap_or(input);
+	let prefixes = [
+		"u16", "u32", "u64", "i16", "i32", "i64", "f16", "f32", "f64", "dec", "dw", "ch", "fp", "dp", "rg", "sz", "st",
+		"fn", "d", "b", "i", "f", "c", "f", "n", "p",
+	];
+
+	let input = prefixes.iter().find_map(|prefix| input.strip_prefix(prefix)).unwrap_or(input);
 
 	for c in input.chars() {
-		if c.is_uppercase() && output.len() > 0 {
+		if c.is_uppercase() && !output.is_empty() {
 			output.push('_');
 			output.push_str(&c.to_lowercase().to_string());
 		} else if c.is_numeric() {
 			output.push(c);
-		} else if c.is_uppercase() && output.len() == 0 {
-			output.push_str(&c.to_lowercase().to_string());
-		} else if c.is_lowercase() {
+		} else {
 			output.push_str(&c.to_lowercase().to_string());
 		}
 	}
